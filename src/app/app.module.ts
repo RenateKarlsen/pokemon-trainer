1c7a6ca7f9b418ed5ca9f6eb9c75dcfee33871bb
Renate Karlsen <renate.karlsen@no.experis.com>
2022-09-23 14:49:42 +0200

    Create trainer, landing and catalogue page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { TrainerPageComponent } from './trainer-page/trainer-page.component';
+import { CataloguePageComponent } from './catalogue-page/catalogue-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    TrainerPageComponent,
+    CataloguePageComponent
   ],
   imports: [
     BrowserModule,
