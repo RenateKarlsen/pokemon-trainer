24ec8972ba82b592bbe68b92bb39700757596748
RK <53771607+RenateKarlsen@users.noreply.github.com>
2022-09-26 13:55:17 +0200

    Routing (#1)
    * Create trainer, landing and catalogue page
    
    * Change pages to type page-component.
    
    * Delete trainerPAGEPAGE
    
    * Add routes
    
    * Add -o to script to open browser automatically
    
    Co-authored-by: Renate Karlsen <renate.karlsen@no.experis.com>
    Co-authored-by: thea.rime <thea.rime@hypatia.no>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrainerPage } from './pages/trainer/trainer.page';
+import { CataloguePage } from './pages/catalogue/catalogue.page';
+import { LandingPage } from './pages/landing/landing.page';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrainerPage,
+    CataloguePage,
+    LandingPage,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: '', component: LandingPage },
+      { path: 'trainer', component: TrainerPage},
+      { path: 'catalogue', component: CataloguePage },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
